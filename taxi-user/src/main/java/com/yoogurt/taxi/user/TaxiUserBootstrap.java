bcd550b4d7d0effccd95928cee9befc31c5413eb
weihao.liu <liu_weihao@sina.cn>
2017-08-30 14:59:19 +0800

    add eureka client

--- taxi-user/src/main/java/com/yoogurt/taxi/user/TaxiUserBootstrap.java
+++ taxi-user/src/main/java/com/yoogurt/taxi/user/TaxiUserBootstrap.java
@@ -3,12 +3,14 @@ package com.yoogurt.taxi.user;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 import java.net.InetAddress;
 
 @Slf4j
+@EnableEurekaClient
 @SpringBootApplication
 public class TaxiUserBootstrap {
 
