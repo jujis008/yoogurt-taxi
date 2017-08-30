a1e3fbbb863af8e651567b1ef59dbbf9c9c78ef9
weihao.liu <liu_weihao@sina.cn>
2017-08-30 10:58:09 +0800

    init auth center

---
+++ auth-center/src/main/java/com/yoogurt/taxi/auth/AuthCenterBootstrap.java
@@ -0,0 +1,31 @@
+package com.yoogurt.taxi.auth;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.net.InetAddress;
+
+@Slf4j
+@SpringBootApplication
+public class AuthCenterBootstrap {
+
+    public static void main(String[] args) throws Exception {
+        ConfigurableApplicationContext context = SpringApplication.run(AuthCenterBootstrap.class, args);
+
+        ConfigurableEnvironment env = context.getEnvironment();
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\thttp://127.0.0.1:{}\n\t" +
+                        "External: \thttp://{}:{}\n\t" +
+                        "----------------------------------------------------------",
+
+                env.getProperty("spring.application.name"),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"));
+    }
+}
