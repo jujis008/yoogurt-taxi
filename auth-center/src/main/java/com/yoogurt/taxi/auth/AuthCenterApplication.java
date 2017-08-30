dc1fe3d889bef4b014166ae1fbd419a5654a1b85
Eric Lau <weihao.liu@gogen.com.cn>
2017-08-30 18:49:24 +0800

    auth center

---
+++ auth-center/src/main/java/com/yoogurt/taxi/auth/AuthCenterApplication.java
@@ -0,0 +1,12 @@
+package com.yoogurt.taxi.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthCenterApplication.class, args);
+	}
+}
