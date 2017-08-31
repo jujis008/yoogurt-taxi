fcfbce5585b03c630faeba2634137552e5855f00
Eric Lau <weihao.liu@gogen.com.cn>
2017-08-31 09:35:26 +0800

    add taxi-system module

---
+++ taxi-system/src/main/java/com/yoogurt/taxi/system/TaxiSystemBootstrap.java
@@ -0,0 +1,31 @@
+package com.yoogurt.taxi.system;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.net.InetAddress;
+
+@Slf4j
+@EnableEurekaClient
+@SpringBootApplication
+public class TaxiSystemBootstrap {
+
+	public static void main(String[] args) throws Exception {
+		ConfigurableApplicationContext context = SpringApplication.run(TaxiSystemBootstrap.class, args);
+		ConfigurableEnvironment env = context.getEnvironment();
+		log.info("\n----------------------------------------------------------\n\t" +
+						"Application '{}' is running! Access URLs:\n\t" +
+						"Local: \t\thttp://127.0.0.1:{}\n\t" +
+						"External: \thttp://{}:{}\n\t" +
+						"----------------------------------------------------------",
+
+				env.getProperty("spring.application.name"),
+				env.getProperty("server.port"),
+				InetAddress.getLocalHost().getHostAddress(),
+				env.getProperty("server.port"));
+	}
+}
---
+++ taxi-system/src/test/java/com/yoogurt/taxi/system/TaxiSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yoogurt.taxi.system;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaxiSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
