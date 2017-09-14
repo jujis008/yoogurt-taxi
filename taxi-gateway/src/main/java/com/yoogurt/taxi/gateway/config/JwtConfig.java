f562ae54708e7ded512d6716abb40ee01718983c
weihao.liu <liu_weihao@sina.cn>
2017-09-14 10:59:51 +0800

    rename

--- taxi-gateway/src/main/java/com/yoogurt/taxi/gateway/config/JwtConfig.java
+++ taxi-gateway/src/main/java/com/yoogurt/taxi/gateway/config/JwtConfig.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Data
 @ConfigurationProperties(prefix = "yoogurt.jwt")
-public class JwtProperties {
+public class JwtConfig {
 
     private String header;
 
