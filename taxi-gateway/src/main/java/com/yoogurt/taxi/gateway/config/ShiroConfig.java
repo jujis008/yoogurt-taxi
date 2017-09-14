5e8aa566c792a01ed153a8e829c9991b8066c412
weihao.liu <liu_weihao@sina.cn>
2017-09-14 17:06:10 +0800

    add UniqueDeviceFilter

--- taxi-gateway/src/main/java/com/yoogurt/taxi/gateway/config/ShiroConfig.java
+++ taxi-gateway/src/main/java/com/yoogurt/taxi/gateway/config/ShiroConfig.java
@@ -1,8 +1,8 @@
 package com.yoogurt.taxi.gateway.config;
 
 import com.google.common.collect.Maps;
-import com.google.common.collect.Sets;
 import com.yoogurt.taxi.gateway.filter.MobileAccessFilter;
+import com.yoogurt.taxi.gateway.filter.UniqueDeviceFilter;
 import com.yoogurt.taxi.gateway.filter.UrlPrivilegeCtrlFilter;
 import com.yoogurt.taxi.gateway.filter.UserTokenFilter;
 import com.yoogurt.taxi.gateway.shiro.ShiroRealm;
@@ -15,15 +15,12 @@ import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.CookieRememberMeManager;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.apache.shiro.web.servlet.SimpleCookie;
-import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 
 import javax.servlet.Filter;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * Description:
@@ -66,6 +63,7 @@ public class ShiroConfig {
         Map<String, Filter> filterMap = Maps.newHashMap();
         //自定义Filter
         filterMap.put("userTokenFilter", getUserTokenFilter());
+        filterMap.put("uniqueDeviceFilter", getUniqueDeviceFilter());
         filterMap.put("urlPrivilegeFilter", getUrlPrivilegeCtrlFilter());
         filterMap.put("mobileAccessFilter", getAccessFilter());
         bean.setFilters(filterMap);
@@ -107,7 +105,8 @@ public class ShiroConfig {
         // noSessionCreation: 要求shiro不创建session
         chains.put("/**.html", "noSessionCreation");
         //需要拦截的移动端uri
-        chains.put("/mobile/**", "noSessionCreation,userTokenFilter,mobileAccessFilter");
+        //uniqueDeviceFilter在userTokenFilter之后
+        chains.put("/mobile/**", "noSessionCreation,userTokenFilter,uniqueDeviceFilter,mobileAccessFilter");
         //需要拦截的后台uri
         chains.put("/web/**", "userTokenFilter,urlPrivilegeFilter");
 
@@ -123,34 +122,50 @@ public class ShiroConfig {
     @Bean("securityManager")
     public SecurityManager getSecurityManager() {
         log.info("初始化SecurityManager，设置ShiroRealm。");
-        /** 开启了rememberMe功能，不要使用缓存，不好驾驭 ￣□￣｜｜*/
-//        shiroRealm.setCacheManager(getRedisCacheManager());
-//        shiroRealm.setCachingEnabled(true);
-//        shiroRealm.setAuthenticationCachingEnabled(true);
-//        shiroRealm.setAuthorizationCachingEnabled(true);
-
         DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager(shiroRealm);
+        //cookie功能加持
         securityManager.setRememberMeManager(getRememberMeManager());
-//        securityManager.setCacheManager(getRedisCacheManager());
-        /** 不要维护session */
-//        securityManager.setSessionManager(new DefaultWebSessionManager());
         return securityManager;
     }
 
-    @Bean(name = "redisCacheManager")
-    public RedisCacheManager getRedisCacheManager() {
-        return new RedisCacheManager();
-    }
-
+    /**
+     * 移动端过滤器，含shiro重新登录的业务逻辑
+     * @return MobileAccessFilter
+     */
     @Bean(name = "mobileAccessFilter")
     public MobileAccessFilter getAccessFilter() {
         return new MobileAccessFilter();
     }
 
+    /**
+     * Token过期验证过滤器
+     * @return UserTokenFilter
+     */
     @Bean(name = "userTokenFilter")
     public UserTokenFilter getUserTokenFilter() {
         return new UserTokenFilter();
     }
+
+    /**
+     * 同账号，多设备互踢过滤器
+     * @return UniqueDeviceFilter
+     */
+    @Bean(name = "uniqueDeviceFilter")
+    public UniqueDeviceFilter getUniqueDeviceFilter() {
+        return new UniqueDeviceFilter();
+    }
+
+    /**
+     * web端过滤器，主要对登录状态的判断
+     * @return UrlPrivilegeCtrlFilter
+     */
+    @Bean(name = "urlPrivilegeCtrlFilter")
+    public UrlPrivilegeCtrlFilter getUrlPrivilegeCtrlFilter() {
+        UrlPrivilegeCtrlFilter ctrlFilter = new UrlPrivilegeCtrlFilter();
+        ctrlFilter.setLoginUrl("/login");
+        return ctrlFilter;
+    }
+
     /**
      * 因为需要开启rememberMe功能，需要注入一个RememberMeManager，
      * 通常情况，使用基于Cookie的实现方式。
@@ -169,10 +184,9 @@ public class ShiroConfig {
         return rememberMeManager;
     }
 
-    @Bean(name = "urlPrivilegeCtrlFilter")
-    public UrlPrivilegeCtrlFilter getUrlPrivilegeCtrlFilter() {
-        UrlPrivilegeCtrlFilter ctrlFilter = new UrlPrivilegeCtrlFilter();
-        ctrlFilter.setLoginUrl("/login");
-        return ctrlFilter;
+    @Bean(name = "redisCacheManager")
+    @Deprecated
+    public RedisCacheManager getRedisCacheManager() {
+        return new RedisCacheManager();
     }
 }
