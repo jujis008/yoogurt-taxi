2d363b822407c5e12d0bf998f6bf4f8ccf7c86db
weihao.liu <liu_weihao@sina.cn>
2017-09-14 12:23:32 +0800

    refresh token

--- taxi-gateway/src/main/java/com/yoogurt/taxi/gateway/controller/AuthController.java
+++ taxi-gateway/src/main/java/com/yoogurt/taxi/gateway/controller/AuthController.java
@@ -1,27 +1,22 @@
 package com.yoogurt.taxi.gateway.controller;
 
-import com.google.common.collect.Maps;
-import com.yoogurt.taxi.common.constant.CacheKey;
 import com.yoogurt.taxi.common.enums.StatusCode;
 import com.yoogurt.taxi.common.vo.ResponseObj;
 import com.yoogurt.taxi.gateway.service.AuthService;
 import com.yoogurt.taxi.gateway.shiro.TokenHelper;
-import com.yoogurt.taxi.gateway.shiro.UserAuthenticationToken;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Description:
- *
+ * 认证接口
  * @Author Eric Lau
  * @Date 2017/9/5.
  */
@@ -33,6 +28,9 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
+    @Autowired
+    private TokenHelper tokenHelper;
+
     @RequestMapping(value = "/token/userId/{userId}/grantCode/{grantCode}/username/{username}", method = RequestMethod.GET)
     public ResponseObj token(@PathVariable("userId") String userId, @PathVariable("grantCode") String grantCode, @PathVariable("username") String username) {
 
@@ -41,5 +39,16 @@ public class AuthController {
             return ResponseObj.success(authToken);
         }
         return ResponseObj.fail(StatusCode.BIZ_FAILED, "授权码不存在或已过期");
+    }
+
+
+    @RequestMapping(value = "/refresh", method = RequestMethod.GET)
+    public ResponseObj refresh(HttpServletRequest request) {
+        String oldToken = tokenHelper.getAuthToken(request);
+        String newToken = authService.refreshToken(oldToken);
+        if (StringUtils.isNoneBlank(newToken)) {
+            return ResponseObj.success(newToken);
+        }
+        return ResponseObj.fail(StatusCode.LOGIN_EXPIRE, StatusCode.LOGIN_EXPIRE.getDetail());
     }
 }
