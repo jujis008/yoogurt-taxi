48f585e64c6c856ed70157a13e84dcd21d475fc4
460181594@qq.com <460181594@qq.com>
2017-09-16 16:44:17 +0800

    add aop

---
+++ taxi-gateway/src/main/java/com/yoogurt/taxi/gateway/aop/InsertAspect.java
@@ -0,0 +1,72 @@
+package com.yoogurt.taxi.gateway.aop;
+
+import com.yoogurt.taxi.common.annotation.Domain;
+import com.yoogurt.taxi.gateway.shiro.ServletHelper;
+import com.yoogurt.taxi.gateway.shiro.TokenHelper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.beanutils.MethodUtils;
+import org.apache.commons.collections.CollectionUtils;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+
+@Slf4j
+@Aspect
+@Component
+public class InsertAspect {
+
+    @Autowired
+    private TokenHelper tokenHelper;
+
+    @Before("execution(* com.yoogurt.taxi.dal.mapper..*..insert*(..)) " +
+            "|| execution(* com.yoogurt.taxi.dal.mapper..*..add*(..))")
+    public void before(JoinPoint joinPoint) throws Throwable {
+
+        Object[] args = joinPoint.getArgs();
+        for (Object arg : args) {
+            if (arg != null) {
+                if (arg instanceof List) {
+                    List<?> argList = (List) arg;
+                    if (!CollectionUtils.isEmpty(argList)) {
+                        for (Object object : argList) {
+                            domainHandle(object);
+                        }
+                    }
+                } else {
+                    domainHandle(arg);
+                }
+            }
+        }
+    }
+
+    private void domainHandle(Object object) throws Exception {
+        //添加了@Domain注解
+        if (object.getClass().isAnnotationPresent(Domain.class)) {
+            Long userId = null;
+            try {
+                userId = tokenHelper.getUserId(ServletHelper.getRequest());
+                if (userId == null) {
+                    userId = tokenHelper.getUserId();
+                }
+            } catch (Exception e) {
+                log.error("获取用户ID失败,{}", e);
+            }
+
+            try {//无特殊情况，以下四个字段一定会出现在Bean内
+                MethodUtils.invokeMethod(object, "setCreator", userId == null ? 0L : userId);
+                MethodUtils.invokeMethod(object, "setGmtCreate", new Date());
+                MethodUtils.invokeMethod(object, "setIsDeleted", Boolean.FALSE);
+                MethodUtils.invokeMethod(object, "setModifier", userId == null ? 0L : userId);
+                MethodUtils.invokeMethod(object, "setGmtModify", new Date());
+            } catch (Exception e) {
+                log.error("设置公共字段失败,{}", e);
+            }
+
+        }
+    }
+}
---
+++ taxi-gateway/src/main/java/com/yoogurt/taxi/gateway/aop/UpdateAspect.java
@@ -0,0 +1,69 @@
+package com.yoogurt.taxi.gateway.aop;
+
+import com.yoogurt.taxi.common.annotation.Domain;
+import com.yoogurt.taxi.gateway.shiro.ServletHelper;
+import com.yoogurt.taxi.gateway.shiro.TokenHelper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.beanutils.MethodUtils;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+@Slf4j
+@Aspect
+@Component
+public class UpdateAspect {
+
+    @Autowired
+    private TokenHelper tokenHelper;
+
+    @Before("execution(* com.yoogurt.taxi.dal.mapper..*..*update*(..))" +
+        "||execution(* com.yoogurt.taxi.dal.mapper..*..*edit*(..))" +
+        "||execution(* com.yoogurt.taxi.dal.mapper..*..*delete*(..))"
+    )
+    public void before(JoinPoint joinPoint) throws Throwable {
+        Object[] args = joinPoint.getArgs();
+        if(args != null && args.length > 0)
+            Arrays.stream(args).forEach(arg -> {
+                if (arg != null) {
+                    if (arg instanceof List) {
+                        List argList = (List) arg;
+                        if (!CollectionUtils.isEmpty(argList)) {
+                            argList.stream().forEach(this::domainHandle);
+                        }
+                    } else {
+                        domainHandle(arg);
+                    }
+                }
+            });
+    }
+
+    private void domainHandle(Object object) {
+        if (object.getClass().isAnnotationPresent(Domain.class)) {
+            Long userId = null;
+            try {
+                userId = tokenHelper.getUserId(ServletHelper.getRequest());
+                if (userId == null) {
+                    userId = tokenHelper.getUserId();
+                }
+            } catch (Exception e) {
+                log.error("获取用户ID失败,{}", e);
+            }
+            try {
+                MethodUtils.invokeMethod(object, "setGmtModify", new Date());
+                MethodUtils.invokeMethod(object, "setModifier", userId == null ? 0L : userId);
+            } catch (Exception e) {
+                log.error("设置公共字段失败,{}", e);
+            }
+        }
+    }
+}
+
