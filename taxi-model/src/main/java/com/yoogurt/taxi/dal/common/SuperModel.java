8ebea22f170c0436d522b892582362d2016915a7
460181594@qq.com <460181594@qq.com>
2017-09-19 13:46:39 +0800

    add column annotation

--- taxi-model/src/main/java/com/yoogurt/taxi/dal/common/SuperModel.java
+++ taxi-model/src/main/java/com/yoogurt/taxi/dal/common/SuperModel.java
@@ -3,6 +3,7 @@ package com.yoogurt.taxi.dal.common;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 import java.util.Date;
 
@@ -14,25 +15,30 @@ public class SuperModel {
     /**
      * 是否删除
      */
+    @Column(name = "is_deleted")
     private Boolean isDeleted;
 
     /**
      * 创建时间
      */
+    @Column(name = "gmt_create")
     private Date gmtCreate;
 
     /**
      * 创建人ID
      */
+    @Column(name = "creator")
     private Long creator;
 
     /**
      * 最后修改时间
      */
+    @Column(name = "gmt_modify")
     private Date gmtModify;
 
     /**
      * 最后修改人ID
      */
+    @Column(name = "modifier")
     private Long modifier;
 }
