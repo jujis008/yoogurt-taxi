e0c5cfa8ed0260d9b24a28be0c42b07e2c37cee9
e <wudy875498@163.com>
2017-09-19 14:01:05 +0800

    实现userServiceImpl

--- taxi-model/src/main/java/com/yoogurt/taxi/dal/mapper/UserInfoMapper.java
+++ taxi-model/src/main/java/com/yoogurt/taxi/dal/mapper/UserInfoMapper.java
@@ -1,7 +1,11 @@
 package com.yoogurt.taxi.dal.mapper;
 
+import com.github.pagehelper.Page;
 import com.yoogurt.taxi.dal.beans.UserInfo;
+import com.yoogurt.taxi.dal.condition.user.UserWLCondition;
+import com.yoogurt.taxi.dal.model.user.UserWLModel;
 import tk.mybatis.mapper.common.Mapper;
 
 public interface UserInfoMapper extends Mapper<UserInfo> {
+    Page<UserWLModel> getUserWebListPage(UserWLCondition condition);
 }
--- taxi-user/src/main/java/com/yoogurt/taxi/user/dao/UserDao.java
+++ taxi-user/src/main/java/com/yoogurt/taxi/user/dao/UserDao.java
@@ -1,8 +1,11 @@
 package com.yoogurt.taxi.user.dao;
 
+import com.github.pagehelper.Page;
 import com.yoogurt.taxi.common.dao.IDao;
 import com.yoogurt.taxi.dal.beans.UserInfo;
+import com.yoogurt.taxi.dal.condition.user.UserWLCondition;
 import com.yoogurt.taxi.dal.mapper.UserInfoMapper;
+import com.yoogurt.taxi.dal.model.user.UserWLModel;
 
 /**
  * Description:
@@ -11,4 +14,5 @@ import com.yoogurt.taxi.dal.mapper.UserInfoMapper;
  * @Date 2017/8/28.
  */
 public interface UserDao extends IDao<UserInfoMapper, UserInfo>{
+    Page<UserWLModel> getUserWebListPage(UserWLCondition condition);
 }
