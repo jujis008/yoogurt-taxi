2de6dd7c5106f239af6057964e03c52342af43fc
e <wudy875498@163.com>
2017-09-20 15:55:20 +0800

    更新service接口

--- taxi-model/src/main/java/com/yoogurt/taxi/dal/mapper/RoleInfoMapper.java
+++ taxi-model/src/main/java/com/yoogurt/taxi/dal/mapper/RoleInfoMapper.java
@@ -1,7 +1,11 @@
 package com.yoogurt.taxi.dal.mapper;
 
 import com.yoogurt.taxi.dal.beans.RoleInfo;
+import com.yoogurt.taxi.dal.model.user.RoleWLModel;
 import tk.mybatis.mapper.common.Mapper;
 
+import java.util.List;
+
 public interface RoleInfoMapper extends Mapper<RoleInfo> {
+    List<RoleWLModel> getRoleWebList();
 }
--- taxi-user/src/main/java/com/yoogurt/taxi/user/dao/RoleAuthorityDao.java
+++ taxi-user/src/main/java/com/yoogurt/taxi/user/dao/RoleAuthorityDao.java
@@ -4,10 +4,13 @@ import com.yoogurt.taxi.common.dao.IDao;
 import com.yoogurt.taxi.dal.beans.RoleAuthorityInfo;
 import com.yoogurt.taxi.dal.mapper.RoleAuthorityInfoMapper;
 import com.yoogurt.taxi.dal.model.user.AuthorityModel;
+import com.yoogurt.taxi.dal.model.user.GroupAuthorityLModel;
 
 import java.util.List;
 
 public interface RoleAuthorityDao extends IDao<RoleAuthorityInfoMapper, RoleAuthorityInfo> {
 
     List<AuthorityModel> getAuthoritiesByRoleId(Long roleId);
+
+    List<GroupAuthorityLModel> getAuthorityListByRoleId(Long roleId);
 }
