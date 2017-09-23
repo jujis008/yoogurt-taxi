dab64c80b40e1f48dafd7271838bff77a2d31162
460181594@qq.com <460181594@qq.com>
2017-09-23 16:35:09 +0800

    getRentList

--- taxi-model/src/main/java/com/yoogurt/taxi/dal/mapper/RentInfoMapper.java
+++ taxi-model/src/main/java/com/yoogurt/taxi/dal/mapper/RentInfoMapper.java
@@ -1,8 +1,16 @@
 package com.yoogurt.taxi.dal.mapper;
 
 import com.yoogurt.taxi.dal.beans.RentInfo;
+import com.yoogurt.taxi.dal.model.order.RentPOIModel;
+import org.apache.ibatis.annotations.Param;
 import tk.mybatis.mapper.common.Mapper;
 import tk.mybatis.mapper.common.MySqlMapper;
 
+import java.util.Date;
+import java.util.List;
+
 public interface RentInfoMapper extends Mapper<RentInfo>, MySqlMapper<RentInfo> {
+
+    List<RentPOIModel> getRentList(@Param("startTime")Date startTime, @Param("endTime") Date endTime, @Param("keywords") String keywords);
+
 }
