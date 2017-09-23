4aa11c29e65d9174b125e06d95760c2c20ece798
460181594@qq.com <460181594@qq.com>
2017-09-23 15:26:21 +0800

    RentDao

---
+++ taxi-order/src/main/java/com/yoogurt/taxi/order/dao/impl/RentDaoImpl.java
@@ -0,0 +1,9 @@
+package com.yoogurt.taxi.order.dao.impl;
+
+import com.yoogurt.taxi.common.dao.BaseDao;
+import com.yoogurt.taxi.dal.beans.RentInfo;
+import com.yoogurt.taxi.dal.mapper.RentInfoMapper;
+import com.yoogurt.taxi.order.dao.RentDao;
+
+public class RentDaoImpl extends BaseDao<RentInfoMapper, RentInfo> implements RentDao {
+}
