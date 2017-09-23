3848e7b4b9b45d130329124a9b6c8f7ace5b2301
460181594@qq.com <460181594@qq.com>
2017-09-23 16:39:32 +0800

    getRentList

--- taxi-order/src/main/java/com/yoogurt/taxi/order/dao/RentDao.java
+++ taxi-order/src/main/java/com/yoogurt/taxi/order/dao/RentDao.java
@@ -3,6 +3,12 @@ package com.yoogurt.taxi.order.dao;
 import com.yoogurt.taxi.common.dao.IDao;
 import com.yoogurt.taxi.dal.beans.RentInfo;
 import com.yoogurt.taxi.dal.mapper.RentInfoMapper;
+import com.yoogurt.taxi.dal.model.order.RentPOIModel;
+
+import java.util.Date;
+import java.util.List;
 
 public interface RentDao extends IDao<RentInfoMapper, RentInfo> {
+
+    List<RentPOIModel> getRentList(Date startTime, Date endTime, String keywords);
 }
--- taxi-order/src/main/java/com/yoogurt/taxi/order/dao/impl/RentDaoImpl.java
+++ taxi-order/src/main/java/com/yoogurt/taxi/order/dao/impl/RentDaoImpl.java
@@ -3,7 +3,23 @@ package com.yoogurt.taxi.order.dao.impl;
 import com.yoogurt.taxi.common.dao.BaseDao;
 import com.yoogurt.taxi.dal.beans.RentInfo;
 import com.yoogurt.taxi.dal.mapper.RentInfoMapper;
+import com.yoogurt.taxi.dal.model.order.RentPOIModel;
 import com.yoogurt.taxi.order.dao.RentDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+
+@Repository
 public class RentDaoImpl extends BaseDao<RentInfoMapper, RentInfo> implements RentDao {
+
+    @Autowired
+    private RentInfoMapper rentInfoMapper;
+
+    @Override
+    public List<RentPOIModel> getRentList(Date startTime, Date endTime, String keywords) {
+        return rentInfoMapper.getRentList(startTime, endTime, keywords);
+    }
 }
+
