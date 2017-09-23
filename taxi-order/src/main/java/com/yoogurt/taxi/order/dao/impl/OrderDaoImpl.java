3ffcf411ad12da575e4cd473cf4718304282d194
460181594@qq.com <460181594@qq.com>
2017-09-23 16:39:10 +0800

    add annotation

--- taxi-order/src/main/java/com/yoogurt/taxi/order/dao/impl/OrderDaoImpl.java
+++ taxi-order/src/main/java/com/yoogurt/taxi/order/dao/impl/OrderDaoImpl.java
@@ -4,6 +4,8 @@ import com.yoogurt.taxi.common.dao.BaseDao;
 import com.yoogurt.taxi.dal.beans.OrderInfo;
 import com.yoogurt.taxi.dal.mapper.OrderInfoMapper;
 import com.yoogurt.taxi.order.dao.OrderDao;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class OrderDaoImpl extends BaseDao<OrderInfoMapper, OrderInfo> implements OrderDao{
 }
