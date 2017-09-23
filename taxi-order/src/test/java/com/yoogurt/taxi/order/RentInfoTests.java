f3aeec46ede50037d610b693cbc998bfa8e6238f
460181594@qq.com <460181594@qq.com>
2017-09-23 16:52:51 +0800

    RentInfoTests

---
+++ taxi-order/src/test/java/com/yoogurt/taxi/order/RentInfoTests.java
@@ -0,0 +1,36 @@
+package com.yoogurt.taxi.order;
+
+import com.yoogurt.taxi.dal.beans.RentInfo;
+import com.yoogurt.taxi.order.form.RentForm;
+import com.yoogurt.taxi.order.service.RentInfoService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RentInfoTests {
+
+    @Autowired
+    private RentInfoService rentInfoService;
+
+    @Test
+    public void addRentInfoTest() {
+        RentForm rentForm = new RentForm();
+        rentForm.setAddress("江干区幸福南路1118号");
+        rentForm.setGiveBackTime(new Date());
+        rentForm.setHandoverTime(new Date());
+        rentForm.setLat(30.263684012);
+        rentForm.setLng(120.365214521);
+        rentForm.setPrice(new BigDecimal(120));
+        rentForm.setUserId(8888L);
+        RentInfo rentInfo = rentInfoService.addRentInfo(rentForm);
+        Assert.assertNotNull("租单发布失败", rentInfo);
+    }
+}
