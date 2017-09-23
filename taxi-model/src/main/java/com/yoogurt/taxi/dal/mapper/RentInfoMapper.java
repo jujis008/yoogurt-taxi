2347277d04dd7776065da0eade0e7e7058760c27
460181594@qq.com <460181594@qq.com>
2017-09-23 15:25:45 +0800

    extends MysqlMapper

--- taxi-model/src/main/java/com/yoogurt/taxi/dal/mapper/RentInfoMapper.java
+++ taxi-model/src/main/java/com/yoogurt/taxi/dal/mapper/RentInfoMapper.java
@@ -2,6 +2,7 @@ package com.yoogurt.taxi.dal.mapper;
 
 import com.yoogurt.taxi.dal.beans.RentInfo;
 import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.common.MySqlMapper;
 
-public interface RentInfoMapper extends Mapper<RentInfo> {
+public interface RentInfoMapper extends Mapper<RentInfo>, MySqlMapper<RentInfo> {
 }
