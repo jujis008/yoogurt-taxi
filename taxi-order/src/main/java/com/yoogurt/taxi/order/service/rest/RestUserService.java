1ce3adaf120c1ae4c3832e2fb3c73021cd177399
460181594@qq.com <460181594@qq.com>
2017-09-23 17:32:52 +0800

    RestUserService

---
+++ taxi-order/src/main/java/com/yoogurt/taxi/order/service/rest/RestUserService.java
@@ -0,0 +1,22 @@
+package com.yoogurt.taxi.order.service.rest;
+
+import com.yoogurt.taxi.dal.beans.CarInfo;
+import com.yoogurt.taxi.dal.beans.DriverInfo;
+import com.yoogurt.taxi.dal.beans.UserInfo;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(value = "taxi-user")
+public interface RestUserService {
+
+    @RequestMapping(value = "/rest/user/userId/{userId}", method = RequestMethod.GET)
+    UserInfo getUserInfoById(@PathVariable(name = "userId") Long userId);
+
+    @RequestMapping(value = "/driver/id/{id}", method = RequestMethod.GET)
+    DriverInfo getDriverInfoById(@PathVariable(name = "driverId") Long driverId);
+
+    @RequestMapping(value = "/car/userId/{userId}", method = RequestMethod.GET)
+    CarInfo getCarInfoByUserId(@PathVariable(name = "userId") Long userId);
+}
