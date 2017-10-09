5a88cac0acc3e2f8c58079a6599eb0c82bcf6964
460181594@qq.com <460181594@qq.com>
2017-10-09 18:06:33 +0800

    add description

--- taxi-order/src/main/java/com/yoogurt/taxi/order/form/PickUpForm.java
+++ taxi-order/src/main/java/com/yoogurt/taxi/order/form/PickUpForm.java
@@ -2,6 +2,7 @@ package com.yoogurt.taxi.order.form;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -15,6 +16,12 @@ public class PickUpForm extends OrderForm {
 
 	@NotNull(message = "请指定车辆状态")
 	private Boolean carStatus;
+
+	/**
+	 * 异常描述
+	 */
+	@Length(max = 200, message = "描述内容不能超过200个字")
+	private String description;
 
 	/**
 	 * 如果车辆状态不正常，可以上传车辆图片
