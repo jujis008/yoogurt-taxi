370aee9d893e0f4ac8f38933c2a43ec7c714c288
460181594@qq.com <460181594@qq.com>
2017-10-09 18:07:54 +0800

    give back

---
+++ taxi-order/src/main/java/com/yoogurt/taxi/order/dao/GiveBackDao.java
@@ -0,0 +1,8 @@
+package com.yoogurt.taxi.order.dao;
+
+import com.yoogurt.taxi.common.dao.IDao;
+import com.yoogurt.taxi.dal.beans.OrderGiveBackInfo;
+import com.yoogurt.taxi.dal.mapper.OrderGiveBackInfoMapper;
+
+public interface GiveBackDao extends IDao<OrderGiveBackInfoMapper, OrderGiveBackInfo> {
+}
---
+++ taxi-order/src/main/java/com/yoogurt/taxi/order/dao/impl/GiveBackDaoImpl.java
@@ -0,0 +1,11 @@
+package com.yoogurt.taxi.order.dao.impl;
+
+import com.yoogurt.taxi.common.dao.BaseDao;
+import com.yoogurt.taxi.dal.beans.OrderGiveBackInfo;
+import com.yoogurt.taxi.dal.mapper.OrderGiveBackInfoMapper;
+import com.yoogurt.taxi.order.dao.GiveBackDao;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class GiveBackDaoImpl extends BaseDao<OrderGiveBackInfoMapper, OrderGiveBackInfo> implements GiveBackDao {
+}
