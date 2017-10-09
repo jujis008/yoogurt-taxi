24a3696c75737b81f7980358b74cabc11964bd2b
460181594@qq.com <460181594@qq.com>
2017-10-09 16:53:32 +0800

    add pictures

--- taxi-order/src/main/java/com/yoogurt/taxi/order/form/PickUpForm.java
+++ taxi-order/src/main/java/com/yoogurt/taxi/order/form/PickUpForm.java
@@ -4,12 +4,21 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
 public class PickUpForm extends OrderForm {
 
+	@NotNull(message = "请指定租单")
+	private Long orderId;
+
 	@NotNull(message = "请指定车辆状态")
 	private Boolean carStatus;
 
+	/**
+	 * 如果车辆状态不正常，可以上传车辆图片
+	 */
+	@Size(max = 4, message = "最多只能上传4张图片")
+	private String pictures;
 }
