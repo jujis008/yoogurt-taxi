9fbf51e33e959a13b0dac6bec7838003429a56a6
460181594@qq.com <460181594@qq.com>
2017-10-09 18:07:42 +0800

    accept

---
+++ taxi-order/src/main/java/com/yoogurt/taxi/order/dao/AcceptDao.java
@@ -0,0 +1,8 @@
+package com.yoogurt.taxi.order.dao;
+
+import com.yoogurt.taxi.common.dao.IDao;
+import com.yoogurt.taxi.dal.beans.OrderAcceptInfo;
+import com.yoogurt.taxi.dal.mapper.OrderAcceptInfoMapper;
+
+public interface AcceptDao extends IDao<OrderAcceptInfoMapper, OrderAcceptInfo> {
+}
---
+++ taxi-order/src/main/java/com/yoogurt/taxi/order/dao/impl/AcceptDaoImpl.java
@@ -0,0 +1,12 @@
+package com.yoogurt.taxi.order.dao.impl;
+
+import com.yoogurt.taxi.common.dao.BaseDao;
+import com.yoogurt.taxi.dal.beans.OrderAcceptInfo;
+import com.yoogurt.taxi.dal.mapper.OrderAcceptInfoMapper;
+import com.yoogurt.taxi.order.dao.AcceptDao;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AcceptDaoImpl extends BaseDao<OrderAcceptInfoMapper, OrderAcceptInfo> implements AcceptDao {
+
+}
