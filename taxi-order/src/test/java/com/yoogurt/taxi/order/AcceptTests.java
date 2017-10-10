12d5143ece1b8e420c5edc5fa9cefe7bc6cb3bea
460181594@qq.com <460181594@qq.com>
2017-10-10 16:19:55 +0800

    Accept tests

---
+++ taxi-order/src/test/java/com/yoogurt/taxi/order/AcceptTests.java
@@ -0,0 +1,32 @@
+package com.yoogurt.taxi.order;
+
+import com.yoogurt.taxi.dal.model.order.AcceptOrderModel;
+import com.yoogurt.taxi.order.form.AcceptForm;
+import com.yoogurt.taxi.order.service.AcceptService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AcceptTests {
+
+    @Autowired
+    private AcceptService acceptService;
+
+    @Test
+    public void doAcceptTest() {
+        AcceptForm acceptForm = new AcceptForm();
+        acceptForm.setCarStatus(Boolean.FALSE);
+        acceptForm.setDescription("大灯有裂缝");
+        acceptForm.setOrderId(17092615073534929L);
+        acceptForm.setPictures(new String[]{"http://yoogurt.aliyun-hz.com/xxxx/p/test.jpg", "http://yoogurt.aliyun-hz.com/xxxx/p/test2.jpg"});
+        AcceptOrderModel model = acceptService.doAccept(acceptForm);
+        Assert.assertNotNull("收车失败！", model);
+    }
+}
