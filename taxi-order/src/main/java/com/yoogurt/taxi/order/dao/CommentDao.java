63b319f25f54f721925edca778fd87ecfcc49486
460181594@qq.com <460181594@qq.com>
2017-10-10 20:16:42 +0800

    comment dao

---
+++ taxi-order/src/main/java/com/yoogurt/taxi/order/dao/CommentDao.java
@@ -0,0 +1,8 @@
+package com.yoogurt.taxi.order.dao;
+
+import com.yoogurt.taxi.common.dao.IDao;
+import com.yoogurt.taxi.dal.beans.OrderCommentInfo;
+import com.yoogurt.taxi.dal.mapper.OrderCommentInfoMapper;
+
+public interface CommentDao extends IDao<OrderCommentInfoMapper, OrderCommentInfo> {
+}
