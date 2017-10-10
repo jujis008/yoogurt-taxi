eb0009270645825f7abab6b57edc623cd947c620
460181594@qq.com <460181594@qq.com>
2017-10-10 20:13:45 +0800

    comment form

--- taxi-order/src/main/java/com/yoogurt/taxi/order/form/CommentForm.java
+++ taxi-order/src/main/java/com/yoogurt/taxi/order/form/CommentForm.java
@@ -2,12 +2,17 @@ package com.yoogurt.taxi.order.form;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
 public class CommentForm extends OrderForm {
+
+	@NotNull(message = "请指定租单")
+	private Long orderId;
 
 	@NotNull(message = "请指定评价的司机")
 	private Long driverId;
@@ -15,10 +20,13 @@ public class CommentForm extends OrderForm {
 	@NotNull(message = "请指定评分")
 	private Integer score;
 
-	private String tagId;
+	@Size(max = 3, message = "最多选3个评价标签")
+	private String[] tagId;
 
-	private String tagName;
+	@Size(max = 3, message = "最多选3个评价标签")
+	private String[] tagName;
 
+	@Length(max = 128, message = "备注信息最多128个字")
 	private String remark;
 
 }
