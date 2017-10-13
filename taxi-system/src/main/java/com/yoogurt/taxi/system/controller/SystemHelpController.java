7eff730135bb839578e41ec2d9ba9c70c148a90f
e <wudy875498@163.com>
2017-10-13 10:26:58 +0800

    枚举类接口

--- taxi-system/src/main/java/com/yoogurt/taxi/system/controller/SystemHelpController.java
+++ taxi-system/src/main/java/com/yoogurt/taxi/system/controller/SystemHelpController.java
@@ -4,14 +4,19 @@ import com.aliyuncs.http.ProtocolType;
 import com.aliyuncs.sts.model.v20150401.AssumeRoleResponse;
 import com.yoogurt.taxi.common.controller.BaseController;
 import com.yoogurt.taxi.common.enums.StatusCode;
+import com.yoogurt.taxi.common.utils.ReflectUtils;
 import com.yoogurt.taxi.common.vo.ResponseObj;
 import com.yoogurt.taxi.system.config.OssConfig;
 import com.yoogurt.taxi.system.config.OssTokenModel;
 import com.yoogurt.taxi.system.service.OssAssumeRoleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("common/system")
@@ -45,5 +50,11 @@ public class SystemHelpController extends BaseController{
         model.setArn(assumeRoleResponse.getAssumedRoleUser().getArn());
         model.setAssumedRoleId(assumeRoleResponse.getAssumedRoleUser().getAssumedRoleId());
         return ResponseObj.success(model);
+    }
+
+    @RequestMapping(value = "/i/getSelectShow/className/{className}", method = RequestMethod.GET, produces = {"application/json;charset=utf-8"})
+    public ResponseObj getSelectShow(@PathVariable(name = "className") String className) {
+        List<Map<String, Object>> enumModels = ReflectUtils.enums(className);
+        return ResponseObj.success(enumModels);
     }
 }
