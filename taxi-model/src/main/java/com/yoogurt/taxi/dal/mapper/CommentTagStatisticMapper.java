28e7eb51efebf4c721678cca2dcfeaad87740a7d
460181594@qq.com <460181594@qq.com>
2017-10-14 14:58:51 +0800

    saveStatistic

--- taxi-model/src/main/java/com/yoogurt/taxi/dal/mapper/CommentTagStatisticMapper.java
+++ taxi-model/src/main/java/com/yoogurt/taxi/dal/mapper/CommentTagStatisticMapper.java
@@ -4,4 +4,6 @@ import com.yoogurt.taxi.dal.beans.CommentTagStatistic;
 import tk.mybatis.mapper.common.Mapper;
 
 public interface CommentTagStatisticMapper extends Mapper<CommentTagStatistic> {
+
+    int saveStatistic(CommentTagStatistic statistic);
 }
