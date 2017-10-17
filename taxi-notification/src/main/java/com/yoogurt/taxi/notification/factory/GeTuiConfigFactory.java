96d7071c1892d523da6ca2f0b063a1f80383fba4
460181594@qq.com <460181594@qq.com>
2017-10-17 16:12:59 +0800

    getui config factory

---
+++ taxi-notification/src/main/java/com/yoogurt/taxi/notification/factory/GeTuiConfigFactory.java
@@ -0,0 +1,9 @@
+package com.yoogurt.taxi.notification.factory;
+
+import com.yoogurt.taxi.notification.config.IGeTuiConfig;
+
+public interface GeTuiConfigFactory {
+
+
+    IGeTuiConfig generateConfig();
+}
--- taxi-notification/src/main/java/com/yoogurt/taxi/notification/factory/GeTuiFactory.java
+++ taxi-notification/src/main/java/com/yoogurt/taxi/notification/factory/GeTuiFactory.java
@@ -1,6 +1,18 @@
 package com.yoogurt.taxi.notification.factory;
 
+import com.yoogurt.taxi.common.utils.SpringContextHolder;
+import com.yoogurt.taxi.dal.enums.UserType;
+
 public class GeTuiFactory {
 
-
+    public static GeTuiConfigFactory getConfigFactory(UserType type) {
+        switch (type) {
+            case USER_APP_AGENT:
+                return SpringContextHolder.getBean("config20Factory");
+            case USER_APP_OFFICE:
+                return SpringContextHolder.getBean("config30Factory");
+            default:
+                return new GeTuiDefaultFactory();
+        }
+    }
 }
