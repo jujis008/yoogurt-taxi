a37813b61a6a14f6c63dec8a77b1823bd21ee08d
460181594@qq.com <460181594@qq.com>
2017-10-17 16:18:04 +0800

    getui config

---
+++ taxi-notification/src/main/java/com/yoogurt/taxi/notification/config/GeTuiDefaultConfig.java
@@ -0,0 +1,29 @@
+package com.yoogurt.taxi.notification.config;
+
+public class GeTuiDefaultConfig implements IGeTuiConfig {
+
+    @Override
+    public String getAppId() {
+        return null;
+    }
+
+    @Override
+    public String getAppKey() {
+        return null;
+    }
+
+    @Override
+    public String getHost() {
+        return null;
+    }
+
+    @Override
+    public String getAppSecret() {
+        return null;
+    }
+
+    @Override
+    public String getMasterSecret() {
+        return null;
+    }
+}
