2c227fc283f11c7d06cdcb271065f6740ba92b68
460181594@qq.com <460181594@qq.com>
2017-10-18 12:39:47 +0800

    add parameters

--- taxi-notification/src/main/java/com/yoogurt/taxi/notification/form/UserBindingForm.java
+++ taxi-notification/src/main/java/com/yoogurt/taxi/notification/form/UserBindingForm.java
@@ -8,8 +8,27 @@ import org.hibernate.validator.constraints.NotBlank;
 @Setter
 public class UserBindingForm {
 
+    /**
+     * 推送SDK生成的设备唯一标识
+     */
     @NotBlank(message = "请指定设备")
     private String clientId;
+
+    /**
+     * 设备类型：android-安卓，iOS-苹果
+     */
+    private String deviceType;
+
+    /**
+     * 设备名称，如iPhone 6s xiaomi4
+     */
+    private String deviceName;
+
+    /**
+     * 操作系统版本号
+     */
+    private String osVersion;
+
 
     private Long userId;
 }
