827ad441a559e342a98a64224bc1b93468d1749d
460181594@qq.com <460181594@qq.com>
2017-10-19 16:15:43 +0800

    SendType

--- taxi-model/src/main/java/com/yoogurt/taxi/dal/enums/SendType.java
+++ taxi-model/src/main/java/com/yoogurt/taxi/dal/enums/SendType.java
@@ -5,14 +5,14 @@ import org.apache.commons.lang.StringUtils;
 
 /**
  * <p class="detail">
- * 功能：透传消息发送类型，以便客服端接受到消息后做下一步操作<br/>
+ * 功能：透传消息发送类型，以便客户端接收到消息后做下一步操作。<br/>
  * <strong>
- *   common-普通消息,
- *   verify-认证消息,
- *   order-订单通知消息,
- *   disobey-违约消息,
- *   traffic-违章消息,
- *   withdraw-提现消息。
+ * common-普通消息,
+ * order-订单通知消息,
+ * disobey-违约消息,
+ * traffic-违章消息,
+ * withdraw-提现消息，
+ * verify-认证消息。
  * </strong>
  * </p>
  *
@@ -23,28 +23,36 @@ import org.apache.commons.lang.StringUtils;
 public enum SendType {
 
     COMMON(1000, "common", "您有一条未读消息，请点击查看"),
-    VERIFY_SUCCESS(2010, "verify_success", "【司机认证通过】恭喜您认证通过，若要发布租单，请先充值押金。"),
-    VERIFY_FAILED(2000, "verify_failed", "【司机未认证通过】您递交的资料未通过审核，请重新上传认证资料。"),
-    ORDER_PAID(3020, "order_paid", "【出货单已支付】您有一笔出货单已支付，请尽快发货。"),
-    ORDER_DELIVERED(3030, "order_delivered", "【采购单已发货】您有一笔采购单已发货，请尽快确认收货。"),
-    ORDER_RECEIVED(3040, "order_received", "【出货单已收货】您有一笔出货单已确认收货，请点击查看。"),
-    ORDER_TIMEOUT(4040, "order_timeout", "【采购单已超时】您有一笔采购单已超时，系统将自动取消该笔订单。"),
-    ORDER_REFUND_SUBMIT(4010, "order_refund_submit", "【退款申请卖家同意退款】您有一笔采购单已申请退款成功，我们将会尽快处理、请耐心等待。"),
-    ORDER_REFUND_APPLIED(4020, "order_refund_applied", "【退款申请平台审核通过】您有一笔退款单已申请退款成功，支付金额将在1-7个工作日内原路退还至您的账户，请注意查收。"),
-    ORDER_REFUND_APPLIED_OFF(4050, "order_refund_applied_off", "【退款申请卖家拒绝退款】您有一笔采购单已申请退款失败,请注意查看."),
-    ORDER_REFUND_DELIVERED(4060, "order_refund_delivered", "【退货单已发货】您有一笔退货单已发货，请尽快确认收货。"),
-    ORDER_REFUND(4030, "order_refund", "【出货单退款成功】您有一笔出货单已退款，请注意查收。"),
-    SHOP_OPENED(5010, "shop_opened", "【店铺已开通】恭喜您，店铺已成功开通，请到您的店铺主页查看权限。");
+    ORDER_RENT(2010, "order_rent", "您的%s消息【%s】已由【%s】接单，点击查看详情。"),
+    ORDER_PAID(2011, "order_paid", "订单【%s】已完成支付。"),
+    ORDER_HANDOVER(2020, "order_handover", "您的交易【%s】车主已交车，点击查看详情。"),
+    ORDER_HANDOVER_REMINDER(2021, "order_handover_reminder", "您的交易【%s】已到达交车时间，请完成交车操作，以免造成不必要的损失。"),
+    ORDER_HANDOVER_REMINDER1(2022, "order_handover_reminder1", "您的交易【%s】距离交车时间还有1小时，请及时完成交车操作。"),
+    ORDER_HANDOVER_UNFINISHED_REMINDER(2023, "order_handover_unfinished_reminder", "您的交易【%s】车主在规定时间内未交车操作，已自动取消，点击查看详情。"),
+    ORDER_GIVE_BACK(2030, "order_give_back", "您的交易【%s】车辆已归还，点击查看详情。"),
+    ORDER_GIVE_BACK_REMINDER(2031, "order_give_back_reminder", "您的交易【%s】已到达还车时间，请完成还车操作以免造成不必要的损失。"),
+    ORDER_GIVE_BACK_REMINDER1(2032, "order_give_back_reminder1", "您的交易【%s】距离还车时间还有1小时，请及时完成还车操作。"),
+    ORDER_FINISH(2040, "order_finish", "您的交易【%s】已完成交易，点击查看详情。"),
+    ORDER_TIMEOUT(2050, "order_timeout", "您的%s消息【%s】超期无人接单，已自动取消，点击查看详情。"),
+    ORDER_CANCEL(2060, "order_cancel", "您的交易【%s】被对方/客服取消，点击查看详情。"),
+    ORDER_REFUND(2070, "order_refund", "您有一笔交易退款，点击查看详情。"),
+    DISOBEY_FINE_IN(3010, "disobey_fine_in", "您收到一笔赔偿金，点击查看详情。"),
+    DISOBEY_FINE_OUT(3020, "disobey_fine_out", "您受到一次违约罚款，点击查看详情。"),
+    TRAFFIC_VIOLATION(4010, "traffic_violation", "您的交易【%s】收到一条违章投诉，请及时处理。"),
+    WITHDRAW_SUCCESS(5010, "withdraw_success", "您的提现申请转账成功，点击查看详情。"),
+    WITHDRAW_FAILED(5011, "withdraw_failed", "您的提现申请转账失败，点击查看详情。"),
+    VERIFY_SUCCESS(6010, "verify_success", "恭喜您认证通过，若要发布租单，请先充值押金。"),
+    VERIFY_FAILED(6000, "verify_failed", "您递交的资料未通过审核，请重新上传认证资料。"),;
 
     private int code;
 
-    private String type;
+    private String name;
 
     private String message;
 
-    SendType(int code, String type, String message) {
+    SendType(int code, String name, String message) {
         this.code = code;
-        this.type = type;
+        this.name = name;
         this.message = message;
     }
 
@@ -52,7 +60,7 @@ public enum SendType {
 
         if (!StringUtils.isBlank(type)) {
             for (SendType userType : SendType.values()) {
-                if (type.equals(userType.getType())) {
+                if (type.equals(userType.getName())) {
                     return userType;
                 }
             }
@@ -64,7 +72,7 @@ public enum SendType {
 
         if (!StringUtils.isBlank(type)) {
             for (SendType userType : SendType.values()) {
-                if (type.equals(userType.getType())) {
+                if (type.equals(userType.getName())) {
                     return userType.getMessage();
                 }
             }
