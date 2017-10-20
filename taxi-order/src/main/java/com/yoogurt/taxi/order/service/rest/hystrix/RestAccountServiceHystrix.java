90fa717d50ff7eaf33d44a5f704b04428b5a0c0c
460181594@qq.com <460181594@qq.com>
2017-10-20 09:11:14 +0800

    Hystrix

--- taxi-order/src/main/java/com/yoogurt/taxi/order/service/rest/hystrix/RestAccountServiceHystrix.java
+++ taxi-order/src/main/java/com/yoogurt/taxi/order/service/rest/hystrix/RestAccountServiceHystrix.java
@@ -3,6 +3,7 @@ package com.yoogurt.taxi.order.service.rest.hystrix;
 import com.yoogurt.taxi.common.enums.StatusCode;
 import com.yoogurt.taxi.common.vo.RestResult;
 import com.yoogurt.taxi.dal.beans.FinanceAccount;
+import com.yoogurt.taxi.dal.vo.ModificationVo;
 import com.yoogurt.taxi.order.service.rest.RestAccountService;
 
 public class RestAccountServiceHystrix implements RestAccountService {
@@ -10,5 +11,10 @@ public class RestAccountServiceHystrix implements RestAccountService {
     @Override
     public RestResult<FinanceAccount> getAccountByUserId(Long userId) {
         return RestResult.fail(StatusCode.BIZ_FAILED, "获取账户信息异常");
+    }
+
+    @Override
+    public RestResult updateAccount(ModificationVo vo) {
+        return RestResult.fail(StatusCode.BIZ_FAILED, "更新账户信息异常");
     }
 }
