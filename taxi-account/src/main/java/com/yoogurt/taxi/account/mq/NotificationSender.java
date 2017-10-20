00d756e8ea6140901799b5f6b8e39a570fe18ee4
460181594@qq.com <460181594@qq.com>
2017-10-20 11:40:37 +0800

    push in taxi-acount

---
+++ taxi-account/src/main/java/com/yoogurt/taxi/account/mq/NotificationSender.java
@@ -0,0 +1,25 @@
+package com.yoogurt.taxi.account.mq;
+
+import com.yoogurt.taxi.dal.bo.PushPayload;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationSender {
+
+    private final static String EXCHANGE = "X-Exchange-Notification";
+
+    private final static String ROUTING_KEY = "topic.notification.push.account";
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    /**
+     * 推送请求进入消息队列。
+     * @param payload 消息负载
+     */
+    public void send(PushPayload payload) {
+        rabbitTemplate.convertAndSend(EXCHANGE, ROUTING_KEY, payload);
+    }
+}
