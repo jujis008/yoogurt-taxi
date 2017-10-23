0ecb21152d0405a034f71fdd575ff5fa26640c34
460181594@qq.com <460181594@qq.com>
2017-10-23 17:29:56 +0800

    mapper

---
+++ taxi-model/src/main/java/com/yoogurt/taxi/dal/mapper/FinanceAppChannelMapper.java
@@ -0,0 +1,7 @@
+package com.yoogurt.taxi.dal.mapper;
+
+import com.yoogurt.taxi.dal.beans.FinanceAppChannel;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface FinanceAppChannelMapper extends Mapper<FinanceAppChannel> {
+}
---
+++ taxi-model/src/main/java/com/yoogurt/taxi/dal/mapper/FinancePayChannelMapper.java
@@ -0,0 +1,7 @@
+package com.yoogurt.taxi.dal.mapper;
+
+import com.yoogurt.taxi.dal.beans.FinancePayChannel;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface FinancePayChannelMapper extends Mapper<FinancePayChannel> {
+}
