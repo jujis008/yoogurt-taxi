f5cdb6852a0aa19c9633e07f0ddaca1596504c25
460181594@qq.com <460181594@qq.com>
2017-10-27 14:18:18 +0800

    add HessianRedisSerializer

--- taxi-common/src/main/java/com/yoogurt/taxi/common/config/RedisConfig.java
+++ taxi-common/src/main/java/com/yoogurt/taxi/common/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.yoogurt.taxi.common.config;
 
+import com.yoogurt.taxi.common.serializer.HessianRedisSerializer;
 import com.yoogurt.taxi.common.serializer.JacksonRedisSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,9 +16,28 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    /**
+     * 基于Jackson技术的序列化器。
+     * 实际上是将对象转换成JSON字符串，可读性强，对用户友好。
+     * 但是需要指定序列化对象的类型，通用性不强。
+     * 如下指定的是Object超类，对于一些复杂对象（对象嵌套），
+     * 最后反序列化出来的会是LinkedHashMap类型，导致类型转换出错。
+     * @return JacksonRedisSerializer
+     */
     @Bean(name = "jacksonRedisSerializer")
-    public RedisSerializer getRedisSerializer() {
+    public RedisSerializer getJacksonSerializer() {
+
         return new JacksonRedisSerializer<>(Object.class);
+    }
+
+    /**
+     * 基于Hessian技术的序列化器，可读性不强，但是通用，
+     * 且性能较好，适合做数据传输。
+     * @return HessianRedisSerializer
+     */
+    @Bean(name = "hessianRedisSerializer")
+    public RedisSerializer getHessianSerializer() {
+        return new HessianRedisSerializer<>();
     }
 
     @Bean(name = "redisTemplate")
@@ -28,7 +48,7 @@ public class RedisConfig {
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new StringRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(getRedisSerializer());
+        template.setHashValueSerializer(getHessianSerializer()); //使用hessian序列化较为通用
         template.afterPropertiesSet();
         return template;
     }
---
+++ taxi-common/src/main/java/com/yoogurt/taxi/common/serializer/HessianRedisSerializer.java
@@ -0,0 +1,71 @@
+package com.yoogurt.taxi.common.serializer;
+
+import com.caucho.hessian.io.Hessian2Input;
+import com.caucho.hessian.io.Hessian2Output;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.SerializationException;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+@Slf4j
+public class HessianRedisSerializer<T> implements RedisSerializer<T> {
+
+    /**
+     * Serialize the given object to binary data.
+     *
+     * @param t object to serialize
+     * @return the equivalent binary data
+     */
+    @Override
+    public byte[] serialize(T t) throws SerializationException {
+        Hessian2Output output = null;
+        ByteArrayOutputStream os;
+        try {
+            os = new ByteArrayOutputStream();
+            output = new Hessian2Output(os);
+            output.setCloseStreamOnClose(true);
+            output.writeObject(t);
+            output.flush();
+            return os.toByteArray();
+        } catch (IOException e) {
+            log.error("[Hessian2]对象序列化失败, {}", e);
+        } finally {
+            try {
+                if(output != null) output.close();
+            } catch (IOException e) {
+                log.error("输出流关闭异常, {}", e);
+            }
+        }
+        return new byte[0];
+    }
+
+    /**
+     * Deserialize an object from the given binary data.
+     *
+     * @param bytes object binary representation
+     * @return the equivalent object instance
+     */
+    @Override
+    public T deserialize(byte[] bytes) throws SerializationException {
+        Hessian2Input input = null;
+        ByteArrayInputStream is;
+        try {
+            is = new ByteArrayInputStream(bytes);
+            input = new Hessian2Input(is);
+            input.setCloseStreamOnClose(true);
+            return (T) input.readObject();
+        } catch (IOException e) {
+            log.error("[Hessian2]对象反序列化失败, {}", e);
+        } finally {
+            try {
+                if(input != null) input.close();
+            } catch (IOException e) {
+                log.error("输入流关闭异常, {}", e);
+            }
+        }
+        return null;
+    }
+}
