190698b230526754ed63500bdd1513d01c82e488
460181594@qq.com <460181594@qq.com>
2017-10-27 16:02:01 +0800

    add taskId

--- taxi-finance/src/main/java/com/yoogurt/taxi/finance/task/PayTask.java
+++ taxi-finance/src/main/java/com/yoogurt/taxi/finance/task/PayTask.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Id;
 import java.io.Serializable;
 
 @Getter
@@ -13,6 +14,9 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PayTask implements Serializable {
+
+	@Id
+	private String taskId;
 
 	private TaskInfo task;
 
