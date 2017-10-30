dea656556fe0b463cbaf88e97c9f9011be814d9b
460181594@qq.com <460181594@qq.com>
2017-10-30 13:53:34 +0800

    add method

--- taxi-common/src/main/java/com/yoogurt/taxi/common/utils/BeanRefUtils.java
+++ taxi-common/src/main/java/com/yoogurt/taxi/common/utils/BeanRefUtils.java
@@ -1,13 +1,20 @@
 package com.yoogurt.taxi.common.utils;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.beanutils.BeanMap;
 import org.apache.commons.lang3.StringUtils;
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
+@Slf4j
 public class BeanRefUtils {
 
     /**
@@ -32,8 +39,76 @@ public class BeanRefUtils {
                 map.put(property, method.invoke(obj));
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("Convert Bean to Map occur exception:{}", e);
         }
         return map;
+    }
+
+
+    /**
+     * <p class="detail">
+     * 功能：把Bean转换成map对象输出，map的key是按照ASCII码从小到大排序（字典序）
+     * </p>
+     * @author liuwh
+     * @param obj 要转换的bean对象
+     * @return SortedMap
+     */
+    public static SortedMap<String, Object> toSortedMap(Object obj) {
+        SortedMap<String, Object> sortedMap = new TreeMap<>();
+        Class<?> clz = obj.getClass();
+        Method[] methods = clz.getMethods();
+        for (Method method : methods) {
+            String methodName = method.getName();
+            if (StringUtils.isNoneBlank(methodName) && methodName.startsWith("get") && !methodName.equals("getClass")) {    //继承了Object的getClass方法，要过滤掉
+                try {
+                    Object value = method.invoke(obj);    //调用Bean的get方法，得到属性值
+                    if (value != null) {
+                        String key = methodName.substring(3);
+                        key = key.substring(0, 1).toLowerCase() + key.substring(1);
+                        sortedMap.put(key, value);
+                    }
+                    log.info("转换结果: {}" + sortedMap.toString());
+                } catch (Exception e) {
+                    log.error("Convert Bean to SortedMap occur exception:{}", e);
+                }
+            }
+        }
+        return sortedMap;
+    }
+
+    /**
+     * <p class="detail">
+     * 功能：根据Bean对象，转成xml格式。
+     * 可以指定根元素名称，其他节点名称即为bean的属性名称。
+     * </p>
+     * @author liuwh
+     * @param obj 要转换的bean对象
+     * @param rootElementName XML根元素名称，默认“xml”
+     * @return Document
+     */
+    public static Document toXml(Object obj, String rootElementName){
+        if (StringUtils.isBlank(rootElementName)) rootElementName = "xml";
+        Element rootElement = DocumentHelper.createElement(rootElementName);
+        Document document = DocumentHelper.createDocument(rootElement);
+        Class<?> clz = obj.getClass();
+        Method[] methods = clz.getMethods();
+        for (Method method : methods) {
+            String methodName = method.getName();
+            if (StringUtils.isNoneBlank(methodName) && methodName.startsWith("get") && !methodName.equals("getClass")) {    //继承了Object的getClass方法，要过滤掉
+                try {
+                    Object value = method.invoke(obj);    //调用Bean的get方法，得到属性值
+                    if (value != null) {
+                        String key = methodName.substring(3);
+                        key = key.substring(0, 1).toLowerCase() + key.substring(1);
+                        Element element = DocumentHelper.createElement(key);
+                        element.setText(value.toString());
+                        rootElement.add(element);
+                    }
+                } catch (Exception e) {
+                    log.error("Convert Bean to XML occur exception:{}", e);
+                }
+            }
+        }
+        return document;
     }
 }
