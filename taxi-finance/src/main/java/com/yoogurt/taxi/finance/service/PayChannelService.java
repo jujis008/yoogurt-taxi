bdf7aebae4c9d0863bbfed97db06577f256ad579
460181594@qq.com <460181594@qq.com>
2017-10-30 10:05:17 +0800

    change return value

--- taxi-finance/src/main/java/com/yoogurt/taxi/finance/service/PayChannelService.java
+++ taxi-finance/src/main/java/com/yoogurt/taxi/finance/service/PayChannelService.java
@@ -1,11 +1,11 @@
 package com.yoogurt.taxi.finance.service;
 
-import com.yoogurt.taxi.dal.doc.finance.Payment;
+import com.yoogurt.taxi.common.vo.ResponseObj;
 import com.yoogurt.taxi.finance.task.PayTask;
 
 import java.util.concurrent.CompletableFuture;
 
 public interface PayChannelService {
 
-    CompletableFuture<Payment> doTask(PayTask payTask);
+    CompletableFuture<ResponseObj> doTask(PayTask payTask);
 }
