b2408d31227cd8f9d6e4aaf1cceb2e9a9d00f4e4
460181594@qq.com <460181594@qq.com>
2017-10-31 14:26:49 +0800

    RestTemplate

--- taxi-finance/src/main/java/com/yoogurt/taxi/finance/TaxiFinanceBootstrap.java
+++ taxi-finance/src/main/java/com/yoogurt/taxi/finance/TaxiFinanceBootstrap.java
@@ -5,8 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.web.client.RestTemplate;
 
 import java.net.InetAddress;
 
@@ -29,5 +31,10 @@ public class TaxiFinanceBootstrap {
 				env.getProperty("server.port"),
 				InetAddress.getLocalHost().getHostAddress(),
 				env.getProperty("server.port"));
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 }
