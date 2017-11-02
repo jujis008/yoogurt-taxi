5f24f188413ec890e13e0daef267feec42a48922
460181594@qq.com <460181594@qq.com>
2017-11-02 19:30:39 +0800

    Event

---
+++ taxi-model/src/main/java/com/yoogurt/taxi/dal/doc/finance/Event.java
@@ -0,0 +1,40 @@
+package com.yoogurt.taxi.dal.doc.finance;
+
+import com.yoogurt.taxi.dal.bo.Notify;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Id;
+
+@Setter
+@Getter
+public class Event<T extends Notify> {
+
+	@Id
+	private String eventId;
+
+	/**
+	 * 事件发生的时间戳
+	 */
+	private long created;
+
+	/**
+	 * pay.succeeded 支付对象，支付成功时触发。
+	 * refund.succeeded 退款对象，退款成功时触发。
+	 * transfer.succeeded 企业支付对象，支付成功时触发。
+	 * batch_transfer.succeeded 批量企业付款对象，批量企业付款成功时触发。
+	 * batch_refund.succeeded 批量退款对象，批量退款成功时触发。
+	 */
+	private String eventType;
+
+	/**
+	 * 回调重试的次数
+	 */
+	private int retryTimes;
+
+	/**
+	 * 回调对象。
+	 */
+	private T data;
+
+}
