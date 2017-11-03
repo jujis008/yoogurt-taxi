cb28358f5c4d981f30beb9340cc217fb3fbdb7a8
460181594@qq.com <460181594@qq.com>
2017-11-03 14:12:30 +0800

    EventTask

---
+++ taxi-finance/src/main/java/com/yoogurt/taxi/finance/task/EventTask.java
@@ -0,0 +1,24 @@
+package com.yoogurt.taxi.finance.task;
+
+import com.yoogurt.taxi.dal.doc.finance.Event;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventTask {
+
+	@Id
+	private String taskId;
+
+	private TaskInfo task;
+
+	private Event event;
+
+}
