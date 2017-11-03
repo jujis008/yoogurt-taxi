5664a148d2d097a57044113d3362ca129bd9ca3c
460181594@qq.com <460181594@qq.com>
2017-11-03 11:00:28 +0800

    limitAmount

--- taxi-order/src/main/java/com/yoogurt/taxi/order/service/impl/HandoverRuleServiceImpl.java
+++ taxi-order/src/main/java/com/yoogurt/taxi/order/service/impl/HandoverRuleServiceImpl.java
@@ -8,6 +8,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -28,7 +29,7 @@ public class HandoverRuleServiceImpl implements HandoverRuleService {
         OrderHandoverRule rule = getRuleInfo();
 
         //没有设置规则
-        if(rule == null) return StringUtils.EMPTY;
+        if (rule == null) return StringUtils.EMPTY;
         String unit = rule.getUnit();
         if ("HOURS".equalsIgnoreCase(unit)) {
             unit = "小时";
@@ -41,14 +42,14 @@ public class HandoverRuleServiceImpl implements HandoverRuleService {
 
     /**
      * 根据交车超时时间，获取违约规则，返回null表示没有违约
-     *  @param milliseconds 交车超出的时间
      *
+     * @param milliseconds 交车超出的时间
      */
     @Override
     public OrderHandoverRule getRuleInfo(long milliseconds) {
         if (milliseconds <= 0) return null;
         OrderHandoverRule rule = getRuleInfo();
-        if(rule == null) return null;
+        if (rule == null) return null;
         TimeUnit unit = TimeUnit.valueOf(rule.getUnit());
         long period = unit.toMillis(rule.getTime());
         //没有超出设置好的违约时限，都视为没有违约
@@ -70,15 +71,18 @@ public class HandoverRuleServiceImpl implements HandoverRuleService {
 
     /**
      * 计算罚款金额。
-     * @param rule 违约对应的规则
-     * @param time 超出交车的时长
+     *
+     * @param rule        违约对应的规则
+     * @param time        超出交车的时长
+     * @param limitAmount 最高处罚金额，传入null表示不限
      */
     @Override
-    public Money calculate(OrderHandoverRule rule, int time) {
-        if(rule == null) return null;
+    public Money calculate(OrderHandoverRule rule, int time, BigDecimal limitAmount) {
+        if (rule == null) return null;
+        Money limitMoney = new Money(limitAmount);
         Money money = new Money(rule.getPrice());
         money.multiplyBy(Math.floor((double) time / rule.getTime()));
-        return money;
+        return limitMoney.compareTo(money) < 0 ? limitMoney : money;
     }
 
 
