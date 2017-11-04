e5fb0a255727068bfcbf1538ebc8a02b9815400c
460181594@qq.com <460181594@qq.com>
2017-11-04 10:11:44 +0800

    NotifyController#notify

--- taxi-finance/src/main/java/com/yoogurt/taxi/finance/controller/mobile/AlipayNotifyController.java
+++ taxi-finance/src/main/java/com/yoogurt/taxi/finance/controller/mobile/AlipayNotifyController.java
@@ -1,10 +1,7 @@
 package com.yoogurt.taxi.finance.controller.mobile;
 
-import com.yoogurt.taxi.dal.doc.finance.Event;
 import com.yoogurt.taxi.finance.service.AlipayService;
-import com.yoogurt.taxi.finance.service.NotifyService;
-import com.yoogurt.taxi.finance.task.EventTask;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.yoogurt.taxi.finance.service.impl.AlipayNotifyServiceImpl;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Map;
 
 /**
  * 程序执行完后必须打印输出“success”（不包含引号）。
@@ -22,13 +18,8 @@ import java.util.Map;
  */
 @RestController
 @RequestMapping("/webhooks/finance")
-public class AlipayNotifyController {
+public class AlipayNotifyController extends NotifyController<AlipayService, AlipayNotifyServiceImpl> {
 
-    @Autowired
-    private AlipayService alipayService;
-
-    @Autowired
-    private NotifyService alipayNotifyService;
 
     /**
      * 支付宝App支付成功的回调接口
@@ -37,25 +28,17 @@ public class AlipayNotifyController {
      * @param response 响应对象
      */
     @RequestMapping(value = "/i/alipay", method = RequestMethod.POST, produces = {"application/json;charset=utf-8"})
-    public void notify(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void alipayNotify(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html; charset=UTF-8");
         PrintWriter out = response.getWriter();
-        Map<String, Object> parameterMap = alipayService.parameterResolve(request);
-        Event event = alipayNotifyService.eventParse(parameterMap);
-        //回调任务构造失败
-        if (event == null) {
-            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-            out.write("fail");
+
+        if (super.notify(request, response)) {//回调成功
+            response.setStatus(HttpServletResponse.SC_OK);
+            out.write("success");
             return;
         }
-        //回调任务提交失败
-        if (alipayNotifyService.submit(event) == null) {
-            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-            out.write("fail");
-            return;
-        }
-        response.setStatus(HttpServletResponse.SC_OK);
-        out.write("success");
+        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        out.write("fail");
         out.flush();
     }
 
--- taxi-finance/src/main/java/com/yoogurt/taxi/finance/controller/mobile/WxNotifyController.java
+++ taxi-finance/src/main/java/com/yoogurt/taxi/finance/controller/mobile/WxNotifyController.java
@@ -1,9 +1,7 @@
 package com.yoogurt.taxi.finance.controller.mobile;
 
-import com.yoogurt.taxi.dal.doc.finance.Event;
-import com.yoogurt.taxi.finance.service.NotifyService;
 import com.yoogurt.taxi.finance.service.WxPayService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.yoogurt.taxi.finance.service.impl.WxNotifyServiceImpl;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,39 +10,30 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Map;
 
+/**
+ * 对后台通知交互时，如果微信收到商户的应答不是成功或超时，微信认为通知失败，
+ * 微信会通过一定的策略定期重新发起通知，尽可能提高通知的成功率，但微信不保证通知最终能成功。
+ * （通知频率为15/15/30/180/1800/1800/1800/1800/3600，单位：秒）
+ * 注意：同样的通知可能会多次发送给商户系统。商户系统必须能够正确处理重复的通知。
+ */
 @RestController
 @RequestMapping("/webhooks/finance")
-public class WxNotifyController {
-
-    @Autowired
-    private WxPayService wxPayService;
-
-    @Autowired
-    private NotifyService wxNotifyService;
+public class WxNotifyController extends NotifyController<WxPayService, WxNotifyServiceImpl> {
 
     @RequestMapping(value = "/i/wx", method = RequestMethod.POST, produces = {"application/json;charset=utf-8"})
     public void wxNotify(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         response.setContentType("text/html; charset=UTF-8");
         PrintWriter out = response.getWriter();
-        Map<String, Object> parameterMap = wxPayService.parameterResolve(request);
-        Event event = wxNotifyService.eventParse(parameterMap);
-        //回调任务构造失败
-        if (event == null) {
+
+        if (super.notify(request, response)) {//回调成功
+            response.setStatus(HttpServletResponse.SC_OK);
+            out.write("success");
+        } else {
             response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             out.write("fail");
-            return;
         }
-        //回调任务提交失败
-        if (wxNotifyService.submit(event) == null) {
-            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-            out.write("fail");
-            return;
-        }
-        response.setStatus(HttpServletResponse.SC_OK);
-        out.write("success");
         out.flush();
     }
 }
