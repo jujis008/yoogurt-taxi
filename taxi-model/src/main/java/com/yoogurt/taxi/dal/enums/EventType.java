bbd243825130ab21d993fd98174e739720ec83d3
460181594@qq.com <460181594@qq.com>
2017-11-04 10:24:34 +0800

    EventType

---
+++ taxi-model/src/main/java/com/yoogurt/taxi/dal/enums/EventType.java
@@ -0,0 +1,35 @@
+package com.yoogurt.taxi.dal.enums;
+
+import lombok.Getter;
+import org.apache.commons.lang3.StringUtils;
+
+@Getter
+public enum EventType {
+
+    PAY_SUCCEEDED("pay.succeeded", "支付成功"), //支付对象，支付成功时触发
+    REFUND_SUCCEEDED("refund.succeeded", "退款成功"), //退款对象，退款成功时触发
+    TRANSFER_SUCCEEDED("transfer.succeeded", "转账成功"), //企业支付对象，支付成功时触发
+    BATCH_TRANSFER_SUCCEEDED("batch_transfer.succeeded", "批量付款成功"), //批量企业付款对象，批量企业付款成功时触发
+    BATCH_REFUND_SUCCEEDED("batch_refund.succeeded", "批量退款成功"), //批量退款对象，批量退款成功时触发
+    ;
+
+    private String code;
+
+    private String name;
+
+    public static EventType getEnumsByCode(String code) {
+        if(StringUtils.isNoneBlank(code)) {
+            for (EventType enums : EventType.values()) {
+                if (code.equals(enums.getCode())) {
+                    return enums;
+                }
+            }
+        }
+        return null;
+    }
+
+    EventType(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+}
