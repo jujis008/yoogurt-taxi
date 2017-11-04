346cd39af17345a132919f0861f7eebc36181844
460181594@qq.com <460181594@qq.com>
2017-11-04 09:24:29 +0800

    AlipayNotifyServiceImpl#eventParse

---
+++ taxi-finance/src/main/java/com/yoogurt/taxi/finance/service/impl/AlipayNotifyServiceImpl.java
@@ -0,0 +1,40 @@
+package com.yoogurt.taxi.finance.service.impl;
+
+import com.yoogurt.taxi.common.utils.RandomUtils;
+import com.yoogurt.taxi.dal.bo.AlipayNotify;
+import com.yoogurt.taxi.dal.doc.finance.Event;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.beanutils.BeanUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Slf4j
+@Service("alipayNotifyService")
+public class AlipayNotifyServiceImpl extends NotifyServiceImpl {
+
+    /**
+     * 解析回调请求，组装EventTask
+     *
+     * @param parameterMap 回调请求
+     * @return EventTask
+     */
+    @Override
+    public Event eventParse(Map<String, Object> parameterMap) {
+        if (parameterMap == null || parameterMap.isEmpty()) return null;
+        //生成一个eventId
+        String eventId = "event_" + RandomUtils.getPrimaryKey();
+        //构造一个 Notify
+        AlipayNotify notify = new AlipayNotify();
+        //构造一个 Event
+        Event<AlipayNotify> event = new Event<>(eventId, notify);
+        try {
+            //将Map中的参数注入到
+            BeanUtils.populate(notify, parameterMap);
+            return event;
+        } catch (Exception e) {
+            log.error("回调参数解析发生异常, {}", e);
+        }
+        return null;
+    }
+}
