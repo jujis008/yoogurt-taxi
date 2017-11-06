74a35a45f01d322515a8fb4e03def3a2de7ccd53
460181594@qq.com <460181594@qq.com>
2017-11-06 11:35:55 +0800

    SimpleRabbitListenerContainerFactory

--- taxi-notification/src/main/java/com/yoogurt/taxi/notification/config/mq/AmqpConfig.java
+++ taxi-notification/src/main/java/com/yoogurt/taxi/notification/config/mq/AmqpConfig.java
@@ -4,11 +4,18 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AmqpConfig {
+
+    @Autowired
+    private ConnectionFactory connectionFactory;
 
     private final static String NOTIFICATION_EXCHANGE_NAME = "X-Exchange-Notification";
 
@@ -60,5 +67,18 @@ public class AmqpConfig {
     @Bean
     Binding smsBinding(Queue smsQueue, TopicExchange exchange) {
         return BindingBuilder.bind(smsQueue).to(exchange).with(NOTIFICATION_SMS_TOPIC);
+    }
+
+
+    /**
+     * issue: no method found for class [B
+     * @return SimpleRabbitListenerContainerFactory
+     */
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory() {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(new Jackson2JsonMessageConverter());
+        return factory;
     }
 }
