f4b78ae341d668590cc92fc310eec4576624a7da
460181594@qq.com <460181594@qq.com>
2017-11-07 18:33:48 +0800

    MessageQueue

--- taxi-finance/src/main/java/com/yoogurt/taxi/finance/mq/impl/PayTaskSender.java
+++ taxi-finance/src/main/java/com/yoogurt/taxi/finance/mq/impl/PayTaskSender.java
@@ -1,9 +1,8 @@
 package com.yoogurt.taxi.finance.mq.impl;
 
+import com.yoogurt.taxi.dal.enums.MessageQueue;
 import com.yoogurt.taxi.finance.mq.TaskSender;
-import com.yoogurt.taxi.finance.task.EventTask;
 import com.yoogurt.taxi.finance.task.PayTask;
-import com.yoogurt.taxi.dal.bo.TaskInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,9 +19,9 @@ public class PayTaskSender implements TaskSender<PayTask> {
     public void send(PayTask payTask) {
         if (payTask != null) {
 
-            TaskInfo task = payTask.getTask();
-            log.info(task.toString());
-            rabbitTemplate.convertAndSend(task.getExchangeName(), task.getRoutingKey(), payTask);
+            MessageQueue messageQueue = payTask.getTask().getMessageQueue();
+            log.info(messageQueue.toString());
+            rabbitTemplate.convertAndSend(messageQueue.getExchange(), messageQueue.getRoutingKey(), payTask);
         }
     }
 }
