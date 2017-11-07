59f0605c45c68fa931b04d5fb858f70f941639bc
460181594@qq.com <460181594@qq.com>
2017-11-07 10:44:28 +0800

    serialize with Hessian

--- taxi-common/src/main/java/com/yoogurt/taxi/common/serializer/HessianRedisSerializer.java
+++ taxi-common/src/main/java/com/yoogurt/taxi/common/serializer/HessianRedisSerializer.java
@@ -1,14 +1,9 @@
 package com.yoogurt.taxi.common.serializer;
 
-import com.caucho.hessian.io.Hessian2Input;
-import com.caucho.hessian.io.Hessian2Output;
+import com.yoogurt.taxi.common.utils.SerializeUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.SerializationException;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 
 @Slf4j
 public class HessianRedisSerializer<T> implements RedisSerializer<T> {
@@ -21,26 +16,7 @@ public class HessianRedisSerializer<T> implements RedisSerializer<T> {
      */
     @Override
     public byte[] serialize(T t) throws SerializationException {
-        if(t == null) return new byte[0];
-        Hessian2Output output = null;
-        ByteArrayOutputStream os;
-        try {
-            os = new ByteArrayOutputStream();
-            output = new Hessian2Output(os);
-            output.setCloseStreamOnClose(true);
-            output.writeObject(t);
-            output.flush();
-            return os.toByteArray();
-        } catch (IOException e) {
-            log.error("[Hessian2]对象序列化失败, {}", e);
-        } finally {
-            try {
-                if(output != null) output.close();
-            } catch (IOException e) {
-                log.error("输出流关闭异常, {}", e);
-            }
-        }
-        return new byte[0];
+        return SerializeUtils.serialize(t);
     }
 
     /**
@@ -51,22 +27,9 @@ public class HessianRedisSerializer<T> implements RedisSerializer<T> {
      */
     @Override
     public T deserialize(byte[] bytes) throws SerializationException {
-        if(bytes == null || bytes.length <= 0) return null;
-        Hessian2Input input = null;
-        ByteArrayInputStream is;
-        try {
-            is = new ByteArrayInputStream(bytes);
-            input = new Hessian2Input(is);
-            input.setCloseStreamOnClose(true);
-            return (T) input.readObject();
-        } catch (IOException e) {
-            log.error("[Hessian2]对象反序列化失败, {}", e);
-        } finally {
-            try {
-                if(input != null) input.close();
-            } catch (IOException e) {
-                log.error("输入流关闭异常, {}", e);
-            }
+        Object o = SerializeUtils.deserialize(bytes);
+        if (o != null) {
+            return (T) o;
         }
         return null;
     }
--- taxi-common/src/main/java/com/yoogurt/taxi/common/utils/SerializeUtils.java
+++ taxi-common/src/main/java/com/yoogurt/taxi/common/utils/SerializeUtils.java
@@ -1,52 +1,68 @@
 package com.yoogurt.taxi.common.utils;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import com.caucho.hessian.io.Hessian2Input;
+import com.caucho.hessian.io.Hessian2Output;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.*;
 
 /**
  * 对象序列化工具类
  */
+@Slf4j
 public class SerializeUtils {
 
     /**
      * 序列化对象
-     * @param obj
-     * @return
+     * @param obj 需要被序列化的对象
+     * @return 序列化字节数组
      */
     public static byte[] serialize(Object obj) {
-        ObjectOutputStream oos;
-        ByteArrayOutputStream baos;
+        if(obj == null) return new byte[0];
+        Hessian2Output output = null;
+        ByteArrayOutputStream os;
         try {
-            if (obj != null) {
-                baos = new ByteArrayOutputStream();
-                oos = new ObjectOutputStream(baos);
-                oos.writeObject(obj);
-                return baos.toByteArray();
+            os = new ByteArrayOutputStream();
+            output = new Hessian2Output(os);
+            output.setCloseStreamOnClose(true);
+            output.writeObject(obj);
+            output.flush();
+            return os.toByteArray();
+        } catch (IOException e) {
+            log.error("[Hessian2]对象序列化失败, {}", e);
+        } finally {
+            try {
+                if(output != null) output.close();
+            } catch (IOException e) {
+                log.error("输出流关闭异常, {}", e);
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
-        return null;
+        return new byte[0];
     }
 
     /**
      * 反序列化对象
      *
-     * @param bytes
-     * @return
+     * @param bytes 字节数组
+     * @return 反序列化出来的对象
      */
-    public static Object unserialize(byte[] bytes) {
-        ByteArrayInputStream bais = null;
+    public static Object deserialize(byte[] bytes) {
+        if(bytes == null || bytes.length <= 0) return null;
+        Hessian2Input input = null;
+        ByteArrayInputStream is;
         try {
-            if (bytes != null && bytes.length > 0) {
-                bais = new ByteArrayInputStream(bytes);
-                ObjectInputStream ois = new ObjectInputStream(bais);
-                return ois.readObject();
+            is = new ByteArrayInputStream(bytes);
+            input = new Hessian2Input(is);
+            input.setCloseStreamOnClose(true);
+            return input.readObject();
+        } catch (IOException e) {
+            log.error("[Hessian2]对象反序列化失败, {}", e);
+        } finally {
+            try {
+                if(input != null) input.close();
+            } catch (IOException e) {
+                log.error("输入流关闭异常, {}", e);
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
         return null;
     }
