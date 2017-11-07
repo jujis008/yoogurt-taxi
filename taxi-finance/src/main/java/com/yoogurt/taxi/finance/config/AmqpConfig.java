b0dc876a563dbe8d642ef06aaf27f0bf043e94ff
460181594@qq.com <460181594@qq.com>
2017-11-07 17:01:50 +0800

    chargeNotifyQueue

--- taxi-finance/src/main/java/com/yoogurt/taxi/finance/config/AmqpConfig.java
+++ taxi-finance/src/main/java/com/yoogurt/taxi/finance/config/AmqpConfig.java
@@ -22,9 +22,19 @@ public class AmqpConfig {
 
     public final static String NOTIFY_TOPIC_EXCHANGE_NAME = "X-Exchange-Notify";
 
+    /**
+     * 订单支付专用队列
+     */
     public final static String PAY_NOTIFY_QUEUE_NAME = "X-Queue-Pay-Notify";
 
-    /** 回调专用 route key */
+    /**
+     * 充值专用队列
+     */
+    public final static String CHARGE_NOTIFY_QUEUE_NAME = "X-Queue-Charge-Notify";
+
+    /**
+     * 回调专用 route key
+     */
     private final static String TOPIC_TASK_NOTIFY = "topic.notify.#";
 
     /**
@@ -46,6 +56,7 @@ public class AmqpConfig {
     TopicExchange topicExchange() {
         return new TopicExchange(NOTIFY_TOPIC_EXCHANGE_NAME);
     }
+
     /**
      * 创建支付消息队列
      *
@@ -59,7 +70,7 @@ public class AmqpConfig {
     /**
      * 交换机和支付消息队列的绑定，并指定route key
      *
-     * @param payQueue    消息队列
+     * @param payQueue       消息队列
      * @param directExchange 消息交换机
      * @return Binding
      */
@@ -69,7 +80,7 @@ public class AmqpConfig {
     }
 
     /**
-     * 创建回调消息队列
+     * 创建支付回调消息队列
      *
      * @return Queue
      */
@@ -81,13 +92,35 @@ public class AmqpConfig {
     /**
      * 交换机和回调消息队列的绑定，并指定route key
      *
-     * @param notifyQueue    消息队列
+     * @param notifyQueue   消息队列
      * @param topicExchange 消息交换机
      * @return Binding
      */
     @Bean
     Binding notifyQueueBinding(Queue notifyQueue, TopicExchange topicExchange) {
         return BindingBuilder.bind(notifyQueue).to(topicExchange).with(TOPIC_TASK_NOTIFY);
+    }
+
+    /**
+     * 创建充值回调消息队列
+     *
+     * @return Queue
+     */
+    @Bean("chargeNotifyQueue")
+    Queue chargeNotifyQueue() {
+        return new Queue(CHARGE_NOTIFY_QUEUE_NAME, true);
+    }
+
+    /**
+     * 交换机和回调消息队列的绑定，并指定route key
+     *
+     * @param chargeNotifyQueue 消息队列
+     * @param topicExchange     消息交换机
+     * @return Binding
+     */
+    @Bean
+    Binding chargeNotifyQueueBinding(Queue chargeNotifyQueue, TopicExchange topicExchange) {
+        return BindingBuilder.bind(chargeNotifyQueue).to(topicExchange).with(TOPIC_TASK_NOTIFY);
     }
 
     @Bean
@@ -102,6 +135,7 @@ public class AmqpConfig {
 
     /**
      * issue: no method found for class [B
+     *
      * @return SimpleRabbitListenerContainerFactory
      */
     @Bean
@@ -112,7 +146,19 @@ public class AmqpConfig {
         return factory;
     }
 
+    /**
+     * 订单支付回调TOPIC
+     * @return TOPIC
+     */
     public static String getPaymentNotifyTopic() {
         return "topic.notify.pay";
+    }
+
+    /**
+     * 用户充值回调TOPIC
+     * @return TOPIC
+     */
+    public static String getChargeNotifyTopic() {
+        return "topic.notify.charge";
     }
 }
