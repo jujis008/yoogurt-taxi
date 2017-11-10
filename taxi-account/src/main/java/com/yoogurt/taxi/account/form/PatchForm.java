a2cf9ffdbccbf362f2d5f869767570b3bb57e6ed
e <wudy875498@163.com>
2017-11-10 15:18:56 +0800

    更新

---
+++ taxi-account/src/main/java/com/yoogurt/taxi/account/form/PatchForm.java
@@ -0,0 +1,10 @@
+package com.yoogurt.taxi.account.form;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PatchForm {
+    private Long billNo;
+}
--- taxi-order/src/main/java/com/yoogurt/taxi/order/controller/mobile/DisobeyController.java
+++ taxi-order/src/main/java/com/yoogurt/taxi/order/controller/mobile/DisobeyController.java
@@ -3,18 +3,12 @@ package com.yoogurt.taxi.order.controller.mobile;
 import com.yoogurt.taxi.common.controller.BaseController;
 import com.yoogurt.taxi.common.enums.StatusCode;
 import com.yoogurt.taxi.common.vo.ResponseObj;
-import com.yoogurt.taxi.dal.beans.OrderDisobeyInfo;
 import com.yoogurt.taxi.dal.condition.order.DisobeyListCondition;
-import com.yoogurt.taxi.order.form.TrafficHandleForm;
 import com.yoogurt.taxi.order.service.DisobeyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/mobile/order")
