8edc4d9ba59702aa991570d4f2115febfc73f8a7
460181594@qq.com <460181594@qq.com>
2017-11-14 18:57:02 +0800

    remove RestOrderService

--- taxi-account/src/main/java/com/yoogurt/taxi/account/service/rest/RestOrderService.java
+++
@@ -1,8 +0,0 @@
-package com.yoogurt.taxi.account.service.rest;
-
-import com.yoogurt.taxi.account.service.rest.hystrix.RestOrderServiceHystrix;
-import org.springframework.cloud.netflix.feign.FeignClient;
-
-@FeignClient(value = "taxi-order", fallback = RestOrderServiceHystrix.class)
-public interface RestOrderService {
-}
