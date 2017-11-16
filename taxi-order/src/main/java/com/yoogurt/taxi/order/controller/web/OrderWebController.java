64c982e0927106ae41a0e5abb9259f063598fb3b
e <wudy875498@163.com>
2017-11-16 16:04:12 +0800

    修改订单详情接口，并增加图片获取接口

--- taxi-order/src/main/java/com/yoogurt/taxi/order/controller/web/OrderWebController.java
+++ taxi-order/src/main/java/com/yoogurt/taxi/order/controller/web/OrderWebController.java
@@ -4,6 +4,7 @@ import com.yoogurt.taxi.common.controller.BaseController;
 import com.yoogurt.taxi.common.enums.StatusCode;
 import com.yoogurt.taxi.common.pager.Pager;
 import com.yoogurt.taxi.common.vo.ResponseObj;
+import com.yoogurt.taxi.dal.beans.CommonResource;
 import com.yoogurt.taxi.dal.beans.OrderDisobeyInfo;
 import com.yoogurt.taxi.dal.beans.OrderTrafficViolationInfo;
 import com.yoogurt.taxi.dal.beans.RentInfo;
@@ -18,13 +19,17 @@ import com.yoogurt.taxi.dal.model.order.CancelOrderModel;
 import com.yoogurt.taxi.order.form.CancelForm;
 import com.yoogurt.taxi.order.form.TrafficHandleForm;
 import com.yoogurt.taxi.order.service.*;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("web/order")
@@ -39,6 +44,8 @@ public class OrderWebController extends BaseController{
     private CancelService cancelService;
     @Autowired
     private DisobeyService disobeyService;
+    @Autowired
+    private CommonResourceService   commonResourceService;
 
     @RequestMapping(value = "list",method = RequestMethod.GET,produces = {"application/json;charset=utf-8"})
     public ResponseObj getList(OrderListCondition condition) {
@@ -54,7 +61,32 @@ public class OrderWebController extends BaseController{
             return ResponseObj.fail(StatusCode.BIZ_FAILED,"请指定订单号");
         }
         Map<String, Object> orderDetails = orderInfoService.getOrderDetails(orderId, null);
+        List<OrderDisobeyInfo> disobeyList = disobeyService.getDisobeyList(orderId, null, null);
+        if (CollectionUtils.isNotEmpty(disobeyList)) {
+            orderDetails.put("disobeyList",disobeyList);
+        }
         return ResponseObj.success(orderDetails);
+    }
+
+    @RequestMapping(value = "/resources/{linkId}/{name}", method = RequestMethod.GET, produces = {"application/json;charset=utf-8"})
+    public ResponseObj getResources(@PathVariable(name = "linkId") String linkId, @PathVariable(name = "name") String name) {
+
+        if (StringUtils.isBlank(linkId)) {
+            return ResponseObj.fail(StatusCode.PARAM_BLANK, "请指定对象");
+        }
+        if (!"order_pick_up_info".equalsIgnoreCase(name) && !"order_accept_info".equalsIgnoreCase(name)) {
+            return ResponseObj.fail(StatusCode.FORM_INVALID, "参数有误");
+        }
+
+//        StringBuilder urlBuilder = new StringBuilder();
+        List<CommonResource> resources = commonResourceService.getResources(linkId, name);
+//        for (int j=0,size=resources.size();j<size;j++) {
+//            CommonResource resource = resources.get(j);
+//            urlBuilder.append(resource.getUrl());
+//            if(j != size - 1) urlBuilder.append(",");
+//        }
+        String stringStream = resources.stream().map(CommonResource::getUrl).collect(Collectors.joining(","));
+        return ResponseObj.success(stringStream);
     }
 
     @RequestMapping(value = "cancel",method = RequestMethod.PATCH,produces = {"application/json;charset=utf-8"})
